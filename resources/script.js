75396c4240a9487e75cc9245870b5917d9a22f67
Sean <tweestidbunny@gmail.com>
2024-08-17 16:26:05 -0400

    Add additional validation for password entry

--- resources/script.js
+++ resources/script.js
@@ -33,18 +33,62 @@ cpw.addEventListener('input', () => {
 
 pw.addEventListener('input', () => {
 
-  if (pw.value.length < 8 || pw.value.length > 16) {
+  pwLength = pw.value.length;
+
+  if (pwLength < 8 || pwLength > 16) {
     pwMsg.classList.remove('good');
   }
 
-  if (pw.value.length > 0 && pw.value.length < 8) {
+  if (pwLength > 0 && pwLength < 8) {
     pwMsg.textContent = 'Must be at least 8 characters';
-  } else if (pw.value.length > 16) {
-    pwMsg.textContent = 'Must be less than 17 characters';
+  } else if (pwLength > 16) {
+    pwMsg.textContent = 'No more than 16 characters';
   } else if (pw.value === '') {
     pwMsg.textContent = 'Password Required';
-  } else {
-    pwMsg.textContent = 'Password Acceptable';
-    pwMsg.classList.add('good');
+  }
+
+  if (pwLength >= 8 && pwLength <= 16) {
+    if (checkLtrs(pw.value)) {
+      pwMsg.textContent = 'Password Acceptable';
+      pwMsg.classList.add('good');
+    } else {
+      pwMsg.textContent = 'Password Must Fit Format';
+    }
   }
 });
+
+function checkSpcChars(item) {
+  const spcChar = ['!', '?', '*', '&', '$', '%']
+
+  return spcChar.includes(item);
+
+}
+
+function checkForLetter(item) {
+  const ltrs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  return ltrs.includes(item.toLowerCase());
+}
+
+function checkLtrs(str) {
+
+  let ltr = 0;
+  let cLtr = 0;
+  let spc = 0;
+  let num = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (+str[i] >= 0) {
+      num++;
+    } else if (checkSpcChars(str[i])) {
+      spc++;
+    } else if (checkForLetter(str[i])) {
+      if (str[i].toUpperCase() === str[i]) {
+        cLtr++;
+      } else {
+        ltr++;
+      }
+    }
+  };
+
+  if ((ltr * cLtr * spc * num) > 0) return true;
+}
